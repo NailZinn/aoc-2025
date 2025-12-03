646de3e05f64114ecf3f77b97ff54f193539e9e4
NailZinn <nailzinnurov17@gmail.com>
2025-12-03 21:23:21 +0300

    day 1: Part Two

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -29,3 +29,51 @@ pub fn solve_1() -> i32 {
 
     result
 }
+
+pub fn solve_2() -> i32 {
+    let rotations = fs::read_to_string("src/day_1/input.txt")
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect::<Vec<String>>();
+
+    let mut dial = 50;
+    let mut result = 0;
+
+    for rotation in rotations {
+        let (direction, steps_str) = rotation.split_at(1);
+        let steps = steps_str.parse::<i32>().unwrap();
+
+        let mut left_to_right_rotation_conversion_offset = 0;
+
+        if dial == 0
+        {
+            if steps < DIAL_SIZE || steps > DIAL_SIZE && steps % DIAL_SIZE != 0
+            {
+                left_to_right_rotation_conversion_offset = DIAL_SIZE;
+            }
+        }
+        else
+        {
+            if (steps - dial) % DIAL_SIZE == 0
+            {
+                left_to_right_rotation_conversion_offset = DIAL_SIZE;
+            }
+            else if steps > dial && steps % DIAL_SIZE != 0 && steps % DIAL_SIZE > dial
+            {
+                left_to_right_rotation_conversion_offset = 2 * DIAL_SIZE;
+            }
+        }
+
+        let unbounded_dial = match direction {
+            "L" => dial + (steps + left_to_right_rotation_conversion_offset) / DIAL_SIZE * DIAL_SIZE - steps % DIAL_SIZE,
+            "R" => dial + steps,
+            _ => 0
+        };
+
+        result += unbounded_dial / DIAL_SIZE;
+        dial = unbounded_dial % DIAL_SIZE;
+    }
+
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod day_1;
 
 fn main() {
-    let solution = day_1::solve_1();
-    println!("{}", solution);
+    let solution_1 = day_1::solve_1();
+    println!("{}", solution_1);
+    let solution_2 = day_1::solve_2();
+    println!("{}", solution_2);
 }
