ec0c4d80a31780ca9e92162bba97c0f8baa2d471
NailZinn <nailzinnurov17@gmail.com>
2025-12-13 21:32:28 +0300

    day 2: Part Two

--- src/day_2/mod.rs
+++ src/day_2/mod.rs
@@ -39,3 +39,52 @@ pub fn solve_1() -> i64 {
 
     result
 }
+
+pub fn solve_2() -> i64 {
+    let ranges = fs::read_to_string("src/day_2/input.txt")
+        .unwrap()
+        .trim()
+        .split(',')
+        .map(|x| { x.split('-').collect::<Vec<&str>>() })
+        .map(|x| { (String::from(x[0]), String::from(x[1])) })
+        .collect::<Vec<(String, String)>>();
+
+    let mut result: i64 = 0;
+
+    for range in ranges {
+        let (left, right) = (range.0.parse::<i64>().unwrap(), range.1.parse::<i64>().unwrap());
+        let mut id = left;
+
+        'ids:
+        while id <= right {
+            let id_string = id.to_string();
+            let id_length = id_string.len();
+
+            'pattern_sizes:
+            for pattern_size in 1..id_length / 2 + 1 {
+
+                if id_length % pattern_size != 0 {
+                    continue;
+                }
+
+                let mut i = pattern_size;
+
+                while i < id_length - pattern_size + 1 {
+                    if id_string.as_bytes()[0..pattern_size] != id_string.as_bytes()[i..i + pattern_size] {
+                        continue 'pattern_sizes;
+                    }
+                    i += pattern_size;
+                }
+
+                result += id;
+                id += 1;
+
+                continue 'ids;
+            }
+
+            id += 1;
+        }
+    }
+
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ mod day_2;
 fn main() {
     let solution_1 = day_2::solve_1();
     println!("{}", solution_1);
+    let solution_2 = day_2::solve_2();
+    println!("{}", solution_2);
 }
