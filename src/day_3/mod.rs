4ed23ca05546368e235e28e86a51760d62a3e073
NailZinn <nailzinnurov17@gmail.com>
2025-12-14 19:01:28 +0300

    day 3: Part Two

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -49,3 +49,44 @@ pub fn solve_1() -> i32 {
 
     result
 }
+
+pub fn solve_2() -> i64 {
+    let banks = read_file();
+
+    let mut result: i64 = 0;
+
+    for bank in banks {
+        let batteries = bank
+            .chars()
+            .map(|x| { x.to_digit(10).unwrap() as i32 })
+            .collect::<Vec<i32>>();
+        let batteries_length = batteries.len();
+
+        let mut offset: i32 = 11;
+        let mut skip: usize = 0;
+
+        while offset >= 0 {
+            let battery_enumeration = batteries
+                .iter()
+                .enumerate()
+                .skip(skip)
+                .take(batteries_length - skip - offset as usize)
+                .map(|(index, &value)| (index, value));
+
+            let (mut current_max_index, mut max) = (0, 0);
+
+            for (index, battery) in battery_enumeration {
+                if battery > max {
+                    (current_max_index, max) = (index, battery);
+                }
+            }
+
+            result += 10_i64.pow(offset as u32) * (max as i64);
+            skip = current_max_index + 1;
+
+            offset -= 1;
+        }
+    }
+
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,6 @@ mod day_3;
 fn main() {
     let solution_1 = day_3::solve_1();
     println!("{}", solution_1);
+    let solution_2 = day_3::solve_2();
+    println!("{}", solution_2);
 }
