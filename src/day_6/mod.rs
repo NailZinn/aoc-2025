4421ff9df249a03ebb66e886c61b8a05bfd9b86a
NailZinn <nailzinnurov17@gmail.com>
2025-12-20 19:44:55 +0300

    day 6: Part Two

--- src/day_6/mod.rs
+++ src/day_6/mod.rs
@@ -1,13 +1,11 @@
 use std::fs;
 
-fn read_input() -> (Vec<Vec<i64>>, Vec<String>) {
+fn read_input() -> (Vec<Vec<i64>>, Vec<char>) {
     let lines: Vec<Vec<String>> = fs::read_to_string("src/day_6/input.txt")
         .unwrap()
         .lines()
         .map(|x| x.split(' ').map(String::from).filter(|x| !x.is_empty()).collect())
         .collect();
-
-    println!("{:?}", lines);
 
     let mut operands: Vec<Vec<i64>> = Vec::new();
 
@@ -19,10 +17,10 @@ fn read_input() -> (Vec<Vec<i64>>, Vec<String>) {
         }
     }
 
-    let operators: Vec<String> = lines
-        .last()
-        .unwrap()
-        .to_vec();
+    let operators: Vec<char> = lines[lines.len() - 1]
+        .iter()
+        .map(|x| x.chars().collect::<Vec<char>>()[0])
+        .collect();
 
     (operands, operators)
 }
@@ -33,10 +31,78 @@ pub fn solve_1() -> i64 {
     operands
         .iter()
         .enumerate()
-        .map(|(i, x)| match operators[i].as_str() {
-            "*" => x.iter().product::<i64>(),
-            "+" => x.iter().sum(),
-            _ => panic!("incorrect operator {}", operators[i])
+        .map(|(i, x)| match operators[i] {
+            '*'=> x.iter().product::<i64>(),
+            '+' => x.iter().sum(),
+            _ => unreachable!()
+        })
+        .sum()
+}
+
+pub fn solve_2() -> i64 {
+    let lines: Vec<String> = fs::read_to_string("src/day_6/input.txt")
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect();
+
+    let operators: Vec<char> = lines[lines.len() - 1]
+        .chars()
+        .filter(|x| !x.is_whitespace())
+        .collect();
+
+    let raw_operand_lines: Vec<Vec<char>> = lines
+        .iter()
+        .take(lines.len() - 1)
+        .map(|x| x.chars().collect())
+        .collect();
+
+    let mut operands: Vec<Vec<i64>> = vec![vec![]];
+    let mut current_operands: Vec<i64> = vec![];
+    let mut column = 0;
+
+    for j in 0..raw_operand_lines[0].len() {
+        let mut is_empty_column = true;
+
+        for i in 0..raw_operand_lines.len() {
+            if !raw_operand_lines[i][j].is_whitespace() {
+                is_empty_column = false;
+                break;
+            }
+        }
+
+        if is_empty_column {
+            for &operand in current_operands.iter() {
+                operands[column].push(operand);
+            }
+            operands.push(Vec::new());
+            column += 1;
+            current_operands.clear();
+            continue;
+        }
+
+        let mut number = 0;
+
+        for i in 0..raw_operand_lines.len() {
+            if let Some(ch) = raw_operand_lines[i].get(j) && !ch.is_whitespace() {
+                number = number * 10 + ch.to_digit(10).unwrap() as i64;
+            }
+        }
+
+        current_operands.push(number);
+    }
+
+    for &operand in current_operands.iter() {
+        operands[column].push(operand);
+    }
+
+    operands
+        .iter()
+        .enumerate()
+        .map(|(i, x)| match operators[i] {
+            '*' => x.iter().product::<i64>(),
+            '+' => x.iter().sum(),
+            _ => unreachable!()
         })
         .sum()
 }
--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,6 @@ mod day_6;
 fn main() {
     let solution_1 = day_6::solve_1();
     println!("{}", solution_1);
+    let solution_2 = day_6::solve_2();
+    println!("{}", solution_2);
 }
