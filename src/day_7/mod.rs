b8fb4f6eab2941d1bd2b0ef2e49e5b17d5572faa
NailZinn <nailzinnurov17@gmail.com>
2025-12-21 13:56:21 +0300

    day 7: Part Two

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::{collections::HashSet, fs, iter::repeat};
 
 fn read_input() -> Vec<Vec<char>> {
     fs::read_to_string("src/day_7/input.txt")
@@ -25,10 +25,10 @@ pub fn solve_1() -> i32 {
             if diagram[i][j] == '^' && beam_indices.contains(&j) {
                 result += 1;
                 beam_indices.remove(&j);
-                if j != 0 {
+                if j > 0 {
                     beam_indices.insert(j - 1);
                 }
-                if j != diagram[0].len() {
+                if j < diagram[0].len() {
                     beam_indices.insert(j + 1);
                 }
             }
@@ -36,4 +36,29 @@ pub fn solve_1() -> i32 {
     }
 
     result
+}
+
+pub fn solve_2() -> i64 {
+    let diagram = read_input();
+
+    let mut tree: Vec<i64> = Vec::from_iter(repeat(0).take(diagram[0].len()));
+    let start_index = diagram[0].iter().position(|&x| x == 'S').unwrap();
+    tree[start_index] = 1;
+
+    for i in 1..diagram.len() {
+        for j in 0..diagram[0].len() {
+            if diagram[i][j] == '^' {
+                if j > 0 {
+                    tree[j - 1] += tree[j];
+                }
+                if j < diagram[0].len() {
+                    tree[j + 1] += tree[j];
+                }
+                tree[j] = 0;
+            }
+        }
+    }
+
+
+    tree.iter().sum()
 }
--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,6 @@ mod day_7;
 fn main() {
     let solution_1 = day_7::solve_1();
     println!("{}", solution_1);
+    let solution_2 = day_7::solve_2();
+    println!("{}", solution_2);
 }
